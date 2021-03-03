de0c735914bad74c09ad25f8853e87ec1962eb4e
Vinicius dos Anjos Benfatti <vinibenfatti@gmail.com>
2021-03-03 14:45:15 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2021,5,15);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferença = dataFimPagamento - dataCorrente;
+
+            string mensagem = "Venvimento em: " + TimeSpanHumanizeExtensions.Humanize(diferença);
+
+            Console.WriteLine(mensagem);
+            
+            Console.ReadLine();
+        }
+
+        }
+    }
+
